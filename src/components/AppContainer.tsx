ed4adc97054fd12f7b6c3fcb295b41ac72ac14f3
Esteban LECLET <estebanleclet@gmail.com>
2024-07-22 19:37:15 +0300

    fix: responsive

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -4,7 +4,7 @@ type AppContainerProps = {
 
 export const AppContainer = ({ children }: AppContainerProps) => {
   return (
-    <div className="bg-dark container mx-auto  min-h-screen w-[640px]">
+    <div className="bg-dark container sm:mx-auto  px-[16px] md:w-[640px]">
       {children}
     </div>
   );
