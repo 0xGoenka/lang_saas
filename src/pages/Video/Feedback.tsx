c4aaf1954a8396546fd10a6b21b642e7f1ce7308
Esteban LECLET <estebanleclet@gmail.com>
2024-08-01 13:29:22 +0300

    added show button and shortcut

--- src/pages/Video/Feedback.tsx
+++ src/pages/Video/Feedback.tsx
@@ -1,17 +1,68 @@
 import { useObservable } from "micro-observables";
 import { useServices } from "../../services/Services";
-import { TertiaryButton } from "../../components/Button";
+import {
+  PrimaryButton,
+  SecondaryButton,
+  TertiaryButton,
+} from "../../components/Button";
 import { decode } from "html-entities";
 import { Rating } from "ts-fsrs";
+import { useState } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 
 export const Feedback = () => {
   const { fsrsService, playerService } = useServices();
   const cardToReview = useObservable(fsrsService.cardToReview);
   const nativeCardToReview = useObservable(fsrsService.nativeCardToReview);
+  const [show, setShow] = useState(false);
+  useHotkeys("A", () => playerService.playVideoAt(), [show]);
+  useHotkeys("S", () => setShow(true), [show]);
+  useHotkeys("D", () => again(), []);
+  useHotkeys("W", () => ok(), []);
+  useHotkeys("E", () => easy(), []);
+  useHotkeys("R", () => hard(), []);
 
-  if (!cardToReview) return <div>No card to review</div>;
+  const again = () => {
+    if (!cardToReview) return;
+    fsrsService.updateCardRating(cardToReview, Rating.Again);
+    playerService.playVideoAt();
+  };
+
+  const ok = () => {
+    if (!cardToReview) return;
+    fsrsService.updateCardRating(cardToReview, Rating.Good);
+    playerService.playVideoAt();
+  };
+
+  const easy = () => {
+    if (!cardToReview) return;
+    fsrsService.updateCardRating(cardToReview, Rating.Easy);
+    playerService.playVideoAt();
+  };
+
+  const hard = () => {
+    if (!cardToReview) return;
+    fsrsService.updateCardRating(cardToReview, Rating.Hard);
+    playerService.playVideoAt();
+  };
+
+  if (!cardToReview) return null;
 
   console.log("Feedback subtitle", cardToReview.subtitle);
+
+  if (!show)
+    return (
+      <>
+        <div className="mb-2">
+          <SecondaryButton onClick={() => playerService.playVideoAt()}>
+            Play Again (A)
+          </SecondaryButton>
+        </div>
+        <PrimaryButton onClick={() => setShow(true)}>
+          Show Answer (S)
+        </PrimaryButton>
+      </>
+    );
 
   return (
     <div className="relative flex flex-col h-[30vh] justify-between">
@@ -28,50 +79,30 @@ export const Feedback = () => {
         <div className="justify-evenly">
           <div className="m-[4px] mb-[8px]">
             <span className="m-[4px]">
-              <TertiaryButton
-                onClick={() => {
-                  fsrsService.updateCardRating(cardToReview, Rating.Again);
-                  playerService.playVideoAt();
-                }}
-              >
-                Too hard
+              <TertiaryButton onClick={again}>
+                Too hard (D)
                 {/* <div className="mr-[8px] text-xs font-medium	">
                   Will reappear in 5mn
                 </div> */}
               </TertiaryButton>
             </span>
             <span className="m-[4px]">
-              <TertiaryButton
-                onClick={() => {
-                  fsrsService.updateCardRating(cardToReview, Rating.Hard);
-                  playerService.playVideoAt();
-                }}
-              >
-                Hard
+              <TertiaryButton onClick={hard}>
+                Hard (R)
                 {/* <div className="text-xs font-medium	">Will reappear in 10mn</div> */}
               </TertiaryButton>
             </span>
           </div>
           <div className="m-[4px]">
             <span className="m-[4px]">
-              <TertiaryButton
-                onClick={() => {
-                  fsrsService.updateCardRating(cardToReview, Rating.Good);
-                  playerService.playVideoAt();
-                }}
-              >
-                Ok
+              <TertiaryButton onClick={ok}>
+                Ok (W)
                 {/* <div className="text-xs font-medium	">Will reappear in 1H</div> */}
               </TertiaryButton>
             </span>
             <span className="m-[4px]">
-              <TertiaryButton
-                onClick={() => {
-                  fsrsService.updateCardRating(cardToReview, Rating.Easy);
-                  playerService.playVideoAt();
-                }}
-              >
-                Easy
+              <TertiaryButton onClick={easy}>
+                Easy (E)
                 {/* <div className="text-xs font-medium	">Won’t reappear</div> */}
               </TertiaryButton>
             </span>
@@ -81,3 +112,7 @@ export const Feedback = () => {
     </div>
   );
 };
+// useHotkeys("D", () => again(), []);
+// useHotkeys("R", () => hard(), []);
+// useHotkeys("W", () => ok(), []);
+// useHotkeys("E", () => easy(), []);
